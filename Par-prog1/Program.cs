520b473a677b1790ce492b5258e707f08a908ee6
Lonatri <gyda-94@hotmail.com>
2025-11-26 13:01:07 +0100

    Feilsøking! Moro

--- Par-prog1/Program.cs
+++ Par-prog1/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Test53");
+﻿
+//Oppgave 1
+//Lag et program som kan konvertere valuta, temperatur eller vekt
+//Du skal kunne få alternativer for hva du vil konvertere,
+//velge hva du vil konvertere til, og så skrive inn en verdi før du får svar i konsollen.
+
+
+Converter();
+void Converter()
+{
+    Console.WriteLine("Choose one:");
+    Console.WriteLine("1. Currency\n2. Temperature\n3. Weight");
+    var menyChoice = Console.ReadKey();
+    Console.Clear();
+
+    if (menyChoice.KeyChar == '1')
+    {
+        CurrencyConverter();
+    }
+    else if (menyChoice.KeyChar == '2')
+    {
+        TempConverter();
+    }
+    else if (menyChoice.KeyChar == '3')
+    {
+        WeightConverter();
+    }
+    else
+    {
+        Console.WriteLine("Please choose 1, 2 or 3");
+    }
+}
+
+int CurrencyConverter()
+{
+    return 11;
+}
+
+int TempConverter()
+{
+    return 12;
+}
+
+int WeightConverter()
+{
+    return 13;
+}
